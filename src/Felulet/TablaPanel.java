3baf43fd33c56f0eb98efac938b3e4fd3ffbdfa5
László Mészáros <meszaros.laszlo2000@edu.bme.hu>
2021-12-08 23:12:52 +0100

    KEZDŐ POZÍCIÓ JÓL KIRAJZOLVA

--- src/Felulet/TablaPanel.java
+++ src/Felulet/TablaPanel.java
@@ -10,45 +10,53 @@ import java.awt.*;
  */
 public class TablaPanel extends JPanel {
     private JPanel ujpanel;
-
     private Palya palya = new Palya();
-
 
     TablaPanel() {
         setLayout(new GridLayout(8, 8));
         setBounds(300,20,800,800);
 
-        for (int i = 0; i <= 63; i++) {
-            ujpanel = new JPanel();
-            add(ujpanel);
-        }
+        int sor = 1;
+        for(int i= 0; i <=63; i++) {
+            if (i % 8 == 0)
+                sor++;
+            if ((i + sor) % 2 == 1){
+                ujpanel = new JPanel();
+                add(ujpanel);
+            }
+            else{
+                for (Mezo m : palya.getMezok()) {
+                    if ((i + 1) == ((m.getSor() - 1) * 8 + m.getOszlop())) {
+                        add(m.getMezoPanel());
+                    }
+                }
+            }
 
+        }
 
         /*
           Mezők színének beállítása
          */
-        int sor = 1;
+        sor = 1;
         for(int i= 0; i <=63; i++) {
 
             if (i % 8 == 0)
                 sor++;
             if ((i + sor) % 2 == 1)
                 getComponent(i).setBackground(Color.BLACK);
-            else
-                getComponent(i).setBackground(new Color(245,245,245));
         }
 
 
-        JPanel temp = new JPanel();
-        for(int i= 0; i <=63; i++) {
-            for (Mezo m : palya.getMezok()) {
-                if ((i+1) == ((m.getSor() - 1) * 8 + m.getOszlop())){
-                    temp = (JPanel) getComponent(i);
-                    temp.add(m.getMezoPanel());
-                }
-            }
-        }
+        /*
+         ******************RANDOM KIIRATASOK**************************
 
+        for (Mezo m : palya.getMezok()) {
+            int szam = (m.getSor() - 1) * 8 + m.getOszlop();
+            System.out.print(szam + " ");
+
+        }
+        System.out.print("\n");
+        System.out.print("\n");
 
         for (Mezo m : palya.getMezok()) {
             if (m.getBabu() != null && m.getBabu().getSzin().equals("feher"))
@@ -57,20 +65,8 @@ public class TablaPanel extends JPanel {
                 System.out.println("fekete: (" + m.getSor() + "," + m.getOszlop() + ")");
         }
 
-
-        /*
-         ******************RANDOM KIIRATASOK**************************
          */
-        for (Mezo m : palya.getMezok()) {
-            int szam = (m.getSor() - 1) * 8 + m.getOszlop();
-            System.out.print(szam + " ");
 
-        }
-        System.out.print("\n");
-        System.out.print("\n");
     }
-
-
-
 
 }
