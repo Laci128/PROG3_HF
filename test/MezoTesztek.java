eea27c12697a16083d7075edc2b7d4b224e03166
László Mészáros <meszaros.laszlo2000@edu.bme.hu>
2021-12-15 18:07:25 +0100

    Mező tesztek (3, de 2 fgv) kész

--- test/MezoTesztek.java
+++ test/MezoTesztek.java
@@ -1,23 +1,68 @@
+import Mukodes.Babu;
 import Mukodes.Mezo;
 import Mukodes.Palya;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class MezoTesztek {
     private Palya palya;
-    private int mezoszam;
-    private Mezo jelenlegiMezo;
+    private Mezo mezo;
+    int mezoszam;
 
     @Before
     public void MezoInit(){
         palya = new Palya();
-
+        for (Mezo m: palya.getMezok())
+            m.setBabu(null);
     }
 
     @Test
     public void MezoSorszam(){
+        mezo = new Mezo(1,1);
+
+        mezoszam = 1;
+        int elvartMezoszam = mezo.Mezoszam();
+
+        Assert.assertEquals("Nem jól számolja ki a Mező a sorszámát a sor és az oszlop számából!",elvartMezoszam, mezoszam);
+    }
+
+    @Test
+    public void KiralyLeszEAtalakit(){
+        mezoszam = 1;
+        for (Mezo m: palya.getMezok()) {
+            if (m.Mezoszam() == mezoszam) {
+                mezo = m;  //(1,1)
+                break;
+            }
+        }
+        Babu feketeBabu = new Babu("fekete");
+        mezo.setBabu(feketeBabu);
+        feketeBabu.setJelenlegiMezo(mezo);
+
+        Babu kiraly = mezo.KiralyLeszE();
+
+        Assert.assertNotNull("Nem alakítja át királlya, pedig fekete bábu van a fehér első sorába!",kiraly);
 
     }
 
+    @Test
+    public void KiralyLeszENemAlakitAt(){
+        mezoszam = 1;
+        for (Mezo m: palya.getMezok()) {
+            if (m.Mezoszam() == mezoszam) {
+                mezo = m;  //(1,1)
+                break;
+            }
+        }
+        Babu feherBabu = new Babu("feher");
+        mezo.setBabu(feherBabu);
+        feherBabu.setJelenlegiMezo(mezo);
+
+        Babu NemKiraly = mezo.KiralyLeszE();
+
+        Assert.assertNull("Átalakítja királlya, pedig fehér bábu van fehér első sorába!",NemKiraly);
+
+    }
 
 }
